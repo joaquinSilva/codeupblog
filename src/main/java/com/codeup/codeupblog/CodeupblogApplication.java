3c3cd48eadb021f2bc347ca8f71f37e64e8097c9
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2019-01-07 12:22:22 -0600

    initial setup and commit

---
+++ src/main/java/com/codeup/codeupblog/CodeupblogApplication.java
@@ -0,0 +1,14 @@
+package com.codeup.codeupblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeupblogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeupblogApplication.class, args);
+    }
+
+}
+
