5149e21dd5651993e013795abb42e2ecb40b24e4
Joaquin Silva <SILVA.JOAQUIN.A.04@gmail.com>
2019-01-15 15:52:31 -0600

    worked on authentication exercise. Created authentication controller, user controller, security configuration, users class, and some html templates. Still need to continue linking everything together.

--- src/main/java/com/codeup/codeupblog/repositories/PostRepository.java
+++ src/main/java/com/codeup/codeupblog/repositories/PostRepository.java
@@ -1,4 +1,5 @@
-package com.codeup.codeupblog.models;
+package com.codeup.codeupblog.repositories;
+import com.codeup.codeupblog.models.Post;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
--- src/main/java/com/codeup/codeupblog/repositories/UserRepository.java
+++ src/main/java/com/codeup/codeupblog/repositories/UserRepository.java
@@ -1,4 +1,5 @@
-package com.codeup.codeupblog.models;
+package com.codeup.codeupblog.repositories;
+import com.codeup.codeupblog.models.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
